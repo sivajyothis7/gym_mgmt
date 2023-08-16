86524159908c9b71cb82701077d9f7c6caf95071
Amalghoshv <amalghoshv@gmail.com>
2023-08-16 15:37:47 +0530

    locker

--- enfono_gym/enfono_gym/doctype/booking_locker/booking_locker.js
+++ enfono_gym/enfono_gym/doctype/booking_locker/booking_locker.js
@@ -2,7 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Booking Locker', {
-	// refresh: function(frm) {
-
-	// }
+	from_date: function (frm) {
+        
+	},	
+	to_date: function (frm) {
+		calcDays(frm)
+			
+		
+    },
+	days: function(frm){
+		totalLockerFees(frm)
+		totalLockerFees(frm)
+		
+		
+	},         
+	locker_fee:function(frm){
+		totalLockerFees(frm)
+		
+	}
+	     
+    // locker_fees: function(frm){
+    //     calcLockerFees(frm);
+    // }
 });
+function calcDays(frm){
+	if(frm.doc.from_date && frm.doc.to_date){
+		let days = frappe.datetime.get_diff(frm.doc.to_date, frm.doc.from_date);
+        frm.set_value('days', days);
+	}
+}
+function totalLockerFees(frm){
+	if(frm.doc.locker_fee && frm.doc.days){
+		let locker_fees = frm.doc.locker_fee * frm.doc.days;
+		frm.set_value('locker_fees', locker_fees);
+	}
+}
