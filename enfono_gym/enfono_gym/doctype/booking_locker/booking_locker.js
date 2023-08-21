3157453e994442c3a6f15313517d8ec2b2f88526
Amalghoshv <amalghoshv@gmail.com>
2023-08-21 14:20:26 +0530

    trainer active

--- enfono_gym/enfono_gym/doctype/booking_locker/booking_locker.js
+++ enfono_gym/enfono_gym/doctype/booking_locker/booking_locker.js
@@ -1,6 +1,5 @@
 // Copyright (c) 2023, siva and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Booking Locker', {
     refresh:function (frm){
         lockerStatus(frm)
